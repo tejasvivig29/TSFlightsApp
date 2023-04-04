e72c35751475c55e9cab76c69882121622b486c0
tejasvivig29 <tejasvivig09@gmail.com>
2023-04-03 23:27:58 -0300

    Creating home and admin components

--- TSFlightsApp/src/app/app.module.ts
+++ TSFlightsApp/src/app/app.module.ts
@@ -1,11 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import {Routes, RouterModule} from '@angular/router';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AdminComponent } from './admin/admin.component';
+
+const routes: Routes=[
+  {path: 'home', component: HomeComponent},
+  {path: 'admin', component: AdminComponent}
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AdminComponent
   ],
   imports: [
     BrowserModule
